24d4aead7d4a44813108c5afeb27dafea1e351a2
yukilzw <334652479@qq.com>
2019-07-14 23:50:32 +0800

    first commit

---
+++ lib/config.dart
@@ -0,0 +1,121 @@
+final List<String> navListData = [
+  '推荐', '英雄联盟', '绝地求生', '王者荣耀', '和平精英', '颜值',
+  '一起看', '户外', 'DNF', '穿越火线', '主机游戏', '二次元', 'DOTA2'
+];
+
+final List<String> swiperPic = [
+  'https://sta-op.douyucdn.cn/douyu-vrp-admin/2019/07/13/c26c2b10bf04ade5e21fab65c7708f27.jpg',
+  'https://sta-op.douyucdn.cn/douyu-vrp-admin/2019/07/04/307548887efc180ccce5dc014a6d90e7.png',
+  'https://sta-op.douyucdn.cn/douyu-vrp-admin/2019/07/05/b5b7eae9eacd2c2327aef910ab815c66.jpg',
+  'https://sta-op.douyucdn.cn/douyu-vrp-admin/2019/06/17/e7caeeeea3686b76b125d7137899da57.png',
+  'https://sta-op.douyucdn.cn/douyu-vrp-admin/2019/03/22/6be01395586150cfa0ab9b9378306c4b.jpg',
+  'https://sta-op.douyucdn.cn/douyu-vrp-admin/2019/07/12/6d6460a5c9e2d3b42e92d84ffc1f5ca8.jpg'
+];
+
+final List<Map<String, Object>> liveData_1 = [
+  {
+    "rid": 910907,
+    "vipId": 0,
+    "roomName": "凉凉小主播回来了",
+    "cate1Id": 0,
+    "cate2Id": 201,
+    "roomSrc": "https://rpic.douyucdn.cn/live-cover/appCovers/2019/06/06/910907_20190606212356_small.jpg",
+    "verticalSrc": "https://rpic.douyucdn.cn/live-cover/appCovers/2019/06/06/910907_20190606212356_big.jpg",
+    "avatar": "https://apic.douyucdn.cn/upload/avatar_v3/201903/5967257f0d064d2fa81aefda1bd239bc_middle.jpg",
+    "nickname": "流口水的小熊猫",
+    "isVertical": 1,
+    "liveCity": "大连市",
+    "isLive": 1,
+    "hn": "138.4万"
+  }, {
+    "rid": 5324159,
+    "vipId": 0,
+    "roomName": "15号晚上八点周年庆啦，欢迎大家",
+    "cate1Id": 0,
+    "cate2Id": 201,
+    "roomSrc": "https://rpic.douyucdn.cn/live-cover/roomCover/2018/10/28/0a539aaf2e6a086155cad8af9a40b80e_big.png",
+    "verticalSrc": "https://rpic.douyucdn.cn/live-cover/roomCover/2018/10/28/0a539aaf2e6a086155cad8af9a40b80e_small.png",
+    "avatar": "https://apic.douyucdn.cn/upload/avatar_v3/201812/71c742ce5a77ff0d8f80a4d14e1ad79e_middle.jpg",
+    "nickname": "白菜mm丶",
+    "isVertical": 1,
+    "liveCity": "无锡市",
+    "isLive": 1,
+    "hn": "429.2万"
+  }, {
+    "rid": 5656277,
+    "vipId": 0,
+    "roomName": "对不起我又没洗头",
+    "cate1Id": 0,
+    "cate2Id": 201,
+    "roomSrc": "https://rpic.douyucdn.cn/live-cover/appCovers/2019/06/30/5656277_20190630183152_small.jpg",
+    "verticalSrc": "https://rpic.douyucdn.cn/live-cover/appCovers/2019/06/30/5656277_20190630183152_big.jpg",
+    "avatar": "https://apic.douyucdn.cn/upload/avatar_v3/201904/c4b0311b534046909aee8c345146f81d_middle.jpg",
+    "nickname": "美羊羊公举",
+    "isVertical": 1,
+    "liveCity": "苏州市",
+    "isLive": 1,
+    "hn": "57.8万"
+  }
+];
+
+final List<Map<String, Object>> liveData_2 = [
+  {
+    "rid": 6597095,
+    "vipId": 0,
+    "roomName": "【唱跳主播】试图温柔的舞蹈主播",
+    "cate1Id": 0,
+    "cate2Id": 311,
+    "roomSrc": "https://rpic.douyucdn.cn/live-cover/appCovers/2019/06/28/6597095_20190628140847_small.jpg",
+    "verticalSrc": "https://rpic.douyucdn.cn/live-cover/appCovers/2019/06/28/6597095_20190628140847_big.jpg",
+    "avatar": "https://apic.douyucdn.cn/upload/avatar_v3/201905/d2797d3e092f4e0c9299e06f5e1197c8_middle.jpg",
+    "nickname": "阿让让丶",
+    "isVertical": 0,
+    "liveCity": "鱼塘",
+    "isLive": 1,
+    "hn": "92.1万"
+  }, {
+    "rid": 968987,
+    "vipId": 0,
+    "roomName": "腿长170会跳舞的模特妹妹呀",
+    "cate1Id": 0,
+    "cate2Id": 311,
+    "roomSrc": "https://rpic.douyucdn.cn/asrpic/190714/968987_638575_7eb39_2_2213.jpg",
+    "verticalSrc": "https://rpic.douyucdn.cn/asrpic/190714/968987_638575_7eb39_1_2213.jpg",
+    "avatar": "https://apic.douyucdn.cn/upload/avatar_v3/201907/17436621fc674cb9b89a679259096fcc_middle.jpg",
+    "nickname": "南妹儿呀",
+    "isVertical": 0,
+    "liveCity": "鱼塘",
+    "isLive": 1,
+    "hn": "25.5万"
+  }, {
+    "rid": 513890,
+    "vipId": 0,
+    "roomName": "山东小甜甜",
+    "cate1Id": 0,
+    "cate2Id": 201,
+    "roomSrc": "https://rpic.douyucdn.cn/asrpic/190714/513890_533927_6d421_2_2216.jpg",
+    "verticalSrc": "https://rpic.douyucdn.cn/asrpic/190714/513890_533927_6d421_1_2216.jpg",
+    "avatar": "https://apic.douyucdn.cn/upload/avatar_v3/201904/e5cbdb948f47446982e0f74c1c305c88_middle.jpg",
+    "nickname": "大美人虞姬",
+    "isVertical": 1,
+    "liveCity": "鱼塘",
+    "isLive": 1,
+    "hn": "38.8万"
+  }, {
+    "rid": 6611509,
+    "vipId": 0,
+    "roomName": "想不出标题 想你",
+    "cate1Id": 0,
+    "cate2Id": 201,
+    "roomSrc": "https://rpic.douyucdn.cn/asrpic/190714/6611509_7155295_5b7b0_2_2211.jpg",
+    "verticalSrc": "https://rpic.douyucdn.cn/asrpic/190714/6611509_7155295_5b7b0_1_2211.jpg",
+    "avatar": "https://apic.douyucdn.cn/upload/avatar_v3/201907/1865ab4d43784bf5b96015702c9b4898_middle.jpg",
+    "nickname": "你的怡宝阿",
+    "isVertical": 1,
+    "liveCity": "孝感市",
+    "isLive": 1,
+    "hn": "4.8万"
+  }
+];
+
+final liveData = liveData_2.addAll(liveData_1);
