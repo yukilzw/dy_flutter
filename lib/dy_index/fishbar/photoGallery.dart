7516894cdb26b7d077ed6160308680c2e9d35cee
yukilzw <334652479@qq.com>
2019-12-25 20:13:32 +0800

    remove package

--- lib/dy_index/fishbar/photoGallery.dart
+++ lib/dy_index/fishbar/photoGallery.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
-import 'package:flutter/widgets.dart';
 
 class GalleryItem {
   GalleryItem({this.id, this.resource });
