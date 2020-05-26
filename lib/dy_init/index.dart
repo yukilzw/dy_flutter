7369ae5408c999ed9e3dc13a6032d89e10a48125
yukilzw <334652479@qq.com>
2020-05-26 18:04:44 +0800

    remove io import

--- lib/dy_init/index.dart
+++ lib/dy_init/index.dart
@@ -1,8 +1,6 @@
 /**
  * @discripe: 载入APP启动页
  */
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
